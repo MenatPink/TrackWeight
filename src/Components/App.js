25a952f76a33c7c30af9cc9cea96639fccdbf7a8
Menat Pink <Menat.Pink@uk.ey.com>
2018-08-17 11:39:26 +0100

    Addition of Weight Counter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./../Styles/App.css";
 import TitleBar from "./TitleBar";
+import WeightTile from "./WeightTile";
 
 class App extends Component {
   render() {
     return (
       <div class="container view-wrapper">
         <TitleBar />
+        <WeightTile />
       </div>
     );
   }
