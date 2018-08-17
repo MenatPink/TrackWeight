0eccc7ea5c12aa7181229e9bda8064a057f14d0e
Menat Pink <Menat.Pink@uk.ey.com>
2018-08-17 10:09:05 +0100

    Initial Commit

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import "./../Styles/App.css";
+import TitleBar from "./TitleBar";
+
+class App extends Component {
+  render() {
+    return <TitleBar />;
+  }
+}
+
+export default App;
---
+++ src/Components/TitleBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function TitleBar() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default TitleBar;
